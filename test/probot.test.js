0d4f057cc81c9f590034fb2693e7ec81d26d779d
Rob van der Leek <robvanderleek@gmail.com>
2019-08-20 09:26:30 +0200

    Make branch name configurable, remove now stuff

--- test/probot.test.js
+++ test/probot.test.js
@@ -12,21 +12,41 @@ beforeEach(() => {
   const app = probot.load(myProbotApp)
 
   app.app = () => 'test'
+  nock.cleanAll()
 })
 
-test('creates a branch when an issue is assigned', async () => {
+function nockAccessToken () {
   nock('https://api.github.com')
     .post('/app/installations/1296032/access_tokens')
     .reply(200, { token: 'test' })
+}
 
+function nockEmptyConfig () {
   nock('https://api.github.com')
-    .get('/repos/robvanderleek/create-issue-branch/git/refs/heads/master')
-    .reply(200, { object: { sha: 123456789 } })
+    .get('/repos/robvanderleek/create-issue-branch/contents/.github/issue-branch.yml')
+    .reply(404)
+}
 
+function nockConfig (base64Yaml) {
+  nock('https://api.github.com')
+    .get('/repos/robvanderleek/create-issue-branch/contents/.github/issue-branch.yml')
+    .reply(200, { content: base64Yaml, encoding: 'base64' })
+}
+
+function nockExistingBranch (name, sha) {
+  nock('https://api.github.com')
+    .get(`/repos/robvanderleek/create-issue-branch/git/refs/heads/${name}`)
+    .reply(200, { object: { sha: sha } })
+}
+
+test('creates a branch when an issue is assigned', async () => {
+  nockAccessToken()
+  nockExistingBranch('master', 123456789)
+  nockEmptyConfig()
   let createEndpointCalled = false
 
   nock('https://api.github.com')
-    .post('/repos/robvanderleek/create-issue-branch/git/refs', (body) => {
+    .post('/repos/robvanderleek/create-issue-branch/git/refs', () => {
       createEndpointCalled = true
       return true
     })
@@ -38,18 +58,13 @@ test('creates a branch when an issue is assigned', async () => {
 })
 
 test('do not create a branch when it already exists', async () => {
-  nock('https://api.github.com')
-    .post('/app/installations/1296032/access_tokens')
-    .reply(200, { token: 'test' })
-
-  nock('https://api.github.com')
-    .get('/repos/robvanderleek/create-issue-branch/git/refs/heads/issue-1-Test_issue')
-    .reply(200, { object: { sha: 987654321 } })
-
+  nockAccessToken()
+  nockExistingBranch('issue-1-Test_issue', 987654321)
+  nockEmptyConfig()
   let createEndpointCalled = false
 
   nock('https://api.github.com')
-    .post('/repos/robvanderleek/create-issue-branch/git/refs', (body) => {
+    .post('/repos/robvanderleek/create-issue-branch/git/refs', () => {
       createEndpointCalled = true
       return true
     })
@@ -60,7 +75,39 @@ test('do not create a branch when it already exists', async () => {
   expect(createEndpointCalled).toBeFalsy()
 })
 
-test('get branch name from issue title', () => {
-  expect(myProbotApp.getBranchNameFromIssue(1, 'Hello world')).toBe('issue-1-Hello_world')
-  expect(myProbotApp.getBranchNameFromIssue(2, 'Test issue...')).toBe('issue-2-Test_issue')
+test('create short branch when configured that way', async () => {
+  nockAccessToken()
+  nockExistingBranch('master', 123456789)
+  nockConfig('YnJhbmNoTmFtZTogc2hvcnQK') // echo "branchName: short" | base64
+  let createEndpointCalled = false
+  let branchRef = ''
+
+  nock('https://api.github.com')
+    .post('/repos/robvanderleek/create-issue-branch/git/refs', (body) => {
+      branchRef = body.ref
+      createEndpointCalled = true
+      return true
+    })
+    .reply(200)
+
+  await probot.receive({ name: 'issues', payload: issueAssignedPayload })
+
+  expect(createEndpointCalled).toBeTruthy()
+  expect(branchRef).toBe('refs/heads/issue-1')
+})
+
+test('get full branch name from issue title', () => {
+  expect(myProbotApp.getFullBranchNameFromIssue(1, 'Hello world')).toBe('issue-1-Hello_world')
+  expect(myProbotApp.getFullBranchNameFromIssue(2, 'Test issue...')).toBe('issue-2-Test_issue')
+})
+
+test('get branch name from issue', async () => {
+  let ctx = { config: () => ({ branchName: 'tiny' }) }
+  expect(await myProbotApp.getBranchNameFromIssue(ctx, 12, 'Hello world')).toBe('i12')
+
+  ctx = { config: () => ({ branchName: 'short' }) }
+  expect(await myProbotApp.getBranchNameFromIssue(ctx, 12, 'Hello world')).toBe('issue-12')
+
+  ctx = { config: () => ({ branchName: 'full' }) }
+  expect(await myProbotApp.getBranchNameFromIssue(ctx, 12, 'Hello world')).toBe('issue-12-Hello_world')
 })
