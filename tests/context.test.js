d9e639606e3c93973addb7771d207261c5cbc0d5
Rob van der Leek <robvanderleek@gmail.com>
2021-01-11 19:39:46 +0100

    Set assignee or sender in empty commit

---
+++ tests/context.test.js
@@ -0,0 +1,21 @@
+const context = require('../src/context')
+const issueAssignedPayload = require('./test-fixtures/issues.assigned.json')
+const issueCommentCreatedPayload = require('./test-fixtures/issue_comment.created.json')
+
+test('get owner', () => {
+  const ctx = { payload: issueAssignedPayload }
+
+  expect(context.getRepoOwner(ctx)).toBe('robvanderleek')
+})
+
+test('get assignee', () => {
+  const ctx = { payload: issueAssignedPayload }
+
+  expect(context.getAssignee(ctx)).toBe('robvanderleek')
+})
+
+test('get sender', () => {
+  const ctx = { payload: issueCommentCreatedPayload }
+
+  expect(context.getSender(ctx)).toBe('robvanderleek')
+});
