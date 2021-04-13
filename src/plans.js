cc14cbfb2449034814b23f9e5b91f23f561ac7fa
Rob van der Leek <robvanderleek@gmail.com>
2021-04-13 09:07:52 +0200

    Improved logging a bit

--- src/plans.js
+++ src/plans.js
@@ -5,13 +5,15 @@ const PRO_PLAN_INTRODUCTION_DATE = new Date('2021-04-07T00:00:00.000Z')
 async function isProPlan (app, ctx) {
   try {
     const id = context.getRepoOwnerId(ctx)
+    const login = context.getRepoOwnerLogin(ctx)
+    app.log(`Checking Marketplace for organization: ${login} ...`)
     const res = await ctx.octokit.apps.getSubscriptionPlanForAccount({ account_id: id })
     const purchase = res.data.marketplace_purchase
     if (purchase.plan.price_model === 'FREE') {
-      app.log('Marketplace purchase found: Free plan')
+      app.log('Found Free plan')
       return false
     } else {
-      app.log('Marketplace purchase found: Pro plan')
+      app.log('Found Pro 💰 plan')
       return true
     }
   } catch (error) {
@@ -29,7 +31,7 @@ async function isActivatedBeforeProPlanIntroduction (app, ctx) {
     datestring = purchase.updated_at
   } catch (error) {
     const login = context.getRepoOwnerLogin(ctx)
-    app.log(`Checking App installation date for organization: ${login}`)
+    app.log('Checking App installation date...')
     const github = await app.auth()
     const installation = await github.apps.getUserInstallation({ username: login })
     app.log(`Found installation date: ${installation.data.created_at}`)
@@ -42,6 +44,5 @@ async function isActivatedBeforeProPlanIntroduction (app, ctx) {
 }
 
 module.exports = {
-  isProPlan: isProPlan,
-  isActivatedBeforeProPlanIntroduction: isActivatedBeforeProPlanIntroduction
+  isProPlan: isProPlan, isActivatedBeforeProPlanIntroduction: isActivatedBeforeProPlanIntroduction
 }
