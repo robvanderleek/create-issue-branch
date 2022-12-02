fed420a828e16e5326ace254fe026313bb21e8e2
Rob van der Leek <robvanderleek@gmail.com>
2022-12-02 23:50:47 +0100

    Added webhook function

---
+++ src/functions/webhook.js
@@ -0,0 +1,30 @@
+const { createProbot } = require('probot')
+const app = require('../probot')
+
+const probot = createProbot()
+const loadingApp = probot.load(app)
+
+exports.handler = async function (event, _) {
+  try {
+    await loadingApp
+
+    // this could will be simpler once we  ship `verifyAndParse()`
+    // see https://github.com/octokit/webhooks.js/issues/379
+    await probot.webhooks.verifyAndReceive({
+      id: event.headers['X-GitHub-Delivery'] || event.headers['x-github-delivery'],
+      name: event.headers['X-GitHub-Event'] || event.headers['x-github-event'],
+      signature: event.headers['X-Hub-Signature-256'] || event.headers['x-hub-signature-256'],
+      payload: JSON.parse(event.body)
+    })
+
+    return {
+      statusCode: 200, body: '{"ok":true}'
+    }
+  } catch (error) {
+    app.log.error(error)
+
+    return {
+      statusCode: error.status || 500, error: 'ooops'
+    }
+  }
+}
