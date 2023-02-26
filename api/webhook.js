1bd691824e882183f03c387ece7b7c78f2bb1251
Rob van der Leek <robvanderleek@gmail.com>
2023-02-26 13:37:08 +0100

    Body already parsed

--- api/webhook.js
+++ api/webhook.js
@@ -8,12 +8,11 @@ const handler = async function (request, response) {
   try {
     await loadingApp
     const eventName = request.headers['X-GitHub-Event'] || request.headers['x-github-event']
-    const payload = JSON.parse(request.body)
     await probot.webhooks.verifyAndReceive({
       id: request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery'],
       name: eventName,
       signature: request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256'],
-      payload: payload
+      payload: request.body
     })
     response.status(200).json({ ok: 'true' })
   } catch (error) {
