5a51a7b310802dda586056c1fd34b7892d6e90cb
Rob van der Leek <robvanderleek@gmail.com>
2023-06-26 23:24:42 +0200

    Add conventionalStyle options gitmoji and semver

--- src/github.js
+++ src/github.js
@@ -407,10 +407,10 @@ async function updatePrTitle (app, ctx, config, pr, labels) {
   const repo = context.getRepoName(ctx)
   const pullNumber = pr.number
   const title = pr.title
-  const semverPrefix = Config.getConventionalPrTitlePrefix(config, labels)
-  const updatedTitle = semverPrefix + ' ' + removeSemverPrefix(title)
+  const conventionalPrefix = Config.getConventionalPrTitlePrefix(config, labels)
+  const updatedTitle = conventionalPrefix + ' ' + removeSemverPrefix(title)
   if (updatedTitle !== title) {
-    app.log.info(`Updating prefix for PR #${pullNumber} in ${owner}/${repo} to: ${semverPrefix}`)
+    app.log.info(`Updating prefix for PR #${pullNumber} in ${owner}/${repo} to: ${conventionalPrefix}`)
     await ctx.octokit.pulls.update({ owner: owner, repo: repo, pull_number: pullNumber, title: updatedTitle })
   }
 }
