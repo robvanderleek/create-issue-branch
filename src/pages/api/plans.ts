f2b26d33e26123fbb804002ba28a1e2980eaf07e
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-02-02 11:47:03 +0100

    Adopt NextJS for serverless functions

---
+++ src/pages/api/plans.ts
@@ -0,0 +1,20 @@
+import {createProbot} from "probot";
+import app from "../../probot";
+import {listAppSubscriptions} from "../../plans";
+import {message} from "../../discord";
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+const probot = createProbot()
+const loadingApp = probot.load(app)
+
+export default async function (_: VercelRequest, response: VercelResponse) {
+    try {
+        await loadingApp
+        const subscriptions = await listAppSubscriptions(probot)
+        await message(subscriptions)
+        response.status(200).json({result: 'OK'})
+    } catch (error: any) {
+        probot.log.error(error)
+        response.status(error.status || 500).json({error: error})
+    }
+}
---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,23 @@
+import {createProbot} from "probot";
+import app from "../../probot";
+import {VercelRequest, VercelResponse} from "@vercel/node";
+
+const probot = createProbot()
+const loadingApp = probot.load(app)
+
+export default async function (request: VercelRequest, response: VercelResponse) {
+    try {
+        await loadingApp;
+        const eventName = request.headers['X-GitHub-Event'] || request.headers['x-github-event'];
+        await probot.webhooks.verifyAndReceive({
+            id: request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery'],
+            name: eventName,
+            signature: request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256'],
+            payload: request.body
+        } as any);
+        response.status(200).json({ok: 'true'});
+    } catch (error: any) {
+        probot.log.error(error);
+        response.status(error.status || 500);
+    }
+}
