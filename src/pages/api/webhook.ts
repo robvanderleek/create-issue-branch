14ddb5c5614cd8fdf53b0f5314567150ae687ca9
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-05-24 15:39:11 +0200

    try again

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -14,13 +14,18 @@ const loadingApp = probot.load(app)
 export default async function (request: VercelRequest, response: VercelResponse) {
     try {
         await loadingApp;
+        console.log(request.headers);
+        console.log(typeof request.body);
+        console.log(request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256']);
+        console.log(request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery']);
         const eventName = request.headers['X-GitHub-Event'] || request.headers['x-github-event'];
-        await probot.webhooks.verifyAndReceive({
-            id: request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery'],
-            name: eventName,
-            signature: request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256'],
-            payload: request.body
-        } as any);
+        console.log(eventName);
+        // await probot.webhooks.verifyAndReceive({
+        //     id: request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery'],
+        //     name: eventName,
+        //     signature: request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256'],
+        //     payload: request.body
+        // } as any);
         response.status(200).json({ok: 'true'});
     } catch (error: any) {
         probot.log.info(error);
