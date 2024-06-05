9b2b7cd3e7d229c563cafaffb389081e6d82fe3f
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-06-05 15:05:09 +0200

    Use NodeJS Request/Response types

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,26 +1,25 @@
 import {createProbot} from "probot";
 import app from "../../probot";
-import {VercelRequest, VercelResponse} from "@vercel/node";
 
 const probot = createProbot()
 const loadingApp = probot.load(app)
 
-export default async function (request: VercelRequest, response: VercelResponse) {
+export async function GET(request: Request) {
     try {
         await loadingApp;
-        const payload = JSON.stringify(request.body);
-        const id = request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery'];
-        const eventName = request.headers['X-GitHub-Event'] || request.headers['x-github-event'];
-        const signature = request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256'];
+        const payload = await request.text()
+        const id = request.headers.get('X-GitHub-Delivery') || request.headers.get('x-github-delivery');
+        const eventName = request.headers.get('X-GitHub-Event') || request.headers.get('x-github-event');
+        const signature = request.headers.get('X-Hub-Signature-256') || request.headers.get('x-hub-signature-256');
         await probot.webhooks.verifyAndReceive({
             id: id,
             name: eventName,
             signature: signature,
             payload: payload
         } as any);
-        response.status(200).json({ok: 'true'});
+        return Response.json({ok: 'true'}, {status: 200});
     } catch (error: any) {
         probot.log.info(error);
-        response.status(error.status || 500);
+        return new Response(null, {status: error.status || 500});
     }
 }
