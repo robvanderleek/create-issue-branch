fdacf6ae414389d68251744cf4eb6da13f1da7a8
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-07-28 15:00:46 +0200

    Staging (#881)

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -5,10 +5,29 @@ import {VercelRequest, VercelResponse} from "@vercel/node";
 const probot = createProbot()
 const loadingApp = probot.load(app)
 
+function getBody(request: VercelRequest): Promise<string> {
+    return new Promise((resolve) => {
+        const bodyParts: Uint8Array[] = [];
+        let body;
+        request.on('data', (chunk) => {
+            bodyParts.push(chunk);
+        }).on('end', async () => {
+            body = Buffer.concat(bodyParts).toString();
+            resolve(body)
+        });
+    });
+}
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
+
 export default async function (request: VercelRequest, response: VercelResponse) {
     try {
         await loadingApp;
-        const payload = JSON.stringify(request.body);
+        const payload = await getBody(request);
         const id = request.headers['X-GitHub-Delivery'] || request.headers['x-github-delivery'];
         const eventName = request.headers['X-GitHub-Event'] || request.headers['x-github-event'];
         const signature = request.headers['X-Hub-Signature-256'] || request.headers['x-hub-signature-256'];
