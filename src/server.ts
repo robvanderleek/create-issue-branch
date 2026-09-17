014bb55d3c884ae5a670e0cc1ce9847850f60e5e
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2026-09-17 14:34:00 +0200

    chore: 🚧 Bye Next :wave:

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import app from "./probot.ts";
 import * as path from "node:path";
 import pino from "pino";
 import {existsSync} from "node:fs";
+import {listAppSubscriptions} from "./plans.ts";
+import {message} from "./discord.ts";
 
 const express = Express();
 
@@ -11,36 +13,48 @@ if (existsSync('.env')) {
     process.loadEnvFile();
 }
 
-async function configureWebhook() {
-    const transport = pino.transport({
-        level: process.env.LOG_LEVEL || 'debug',
-        target: 'pino-pretty',
-        options: {
-            colorize: false,
-            sync: true
-        }
-    });
+const transport = pino.transport({
+    level: process.env.LOG_LEVEL || 'debug',
+    target: 'pino-pretty',
+    options: {
+        colorize: false,
+        sync: true
+    }
+});
 
+const probot = createProbot({
+    env: {
+        APP_ID: process.env.APP_ID,
+        PRIVATE_KEY: process.env.PRIVATE_KEY,
+        WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
+    },
+    defaults: {
+        log: pino(transport)
+    }
+});
+
+async function configureWebhook() {
     const middleware = await createNodeMiddleware(app, {
         webhooksPath: "/api/github/webhooks",
-        probot: createProbot({
-            env: {
-                APP_ID: process.env.APP_ID,
-                PRIVATE_KEY: process.env.PRIVATE_KEY,
-                WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
-            },
-            defaults: {
-                log: pino(transport)
-            }
-        }),
+        probot
     });
-
     express.use(middleware);
 }
 
+function configurePlans() {
+    express.get("/plans", async (_, res) => {
+        const octokit = await probot.auth();
+        const subscriptions = await listAppSubscriptions(octokit);
+        await message(subscriptions)
+        res.status(200).json({result: 'OK'})
+    });
+}
+
 await configureWebhook();
+
 express.use(Express.json());
 express.use(Express.static(path.join(import.meta.dirname, '..', 'public')));
+configurePlans();
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const host = process.env.HOST || '0.0.0.0';
